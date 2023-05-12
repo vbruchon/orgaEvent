4d1578416460410ace627c14ed149e3939ca25ab
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-05-12 14:04:15 +0200

    add link to create event in dashboard nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,13 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                   
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('dashboard.create')" :active="request()->routeIs('dashboard.create')">
+                        {{ __('Ajouter un événement') }}
+                    </x-nav-link>
+                   
                 </div>
             </div>
 
