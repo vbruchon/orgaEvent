207216942999dc43a2acc6a45ead8d3ca15463be
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-05-16 11:13:13 +0200

    Change view dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,13 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h2 class="text-xl mb-6">Liste des événements</h2>
-
-                    @if($events->count() > 0)
-                    <ul>
-                        @foreach($events as $event)
-                        <li>{{ $event->name }}</li>
-                        @endforeach
-                    </ul>
-                    @else
-                    <p>Aucun événement</p>
-                    @endif
+                    <h2 class="text-xl mb-6">Bienvenue sur votre tableau de bord</h2>
+                    <p>Ici, vous pouvez :</p>
+                    <div class="flex flex-col">
+                        <a href="{{ route('event.list') }}" class="w-80 ml-5 mb-3 text-xl text-center text-white rounded-lg p-5 bg-fuchsia-900">Consulter les événements</a>
+                        <a href="{{ route('add.event') }}" class="w-80 ml-5 mb-3 text-xl text-center text-white rounded-lg p-5 bg-fuchsia-900">Ajouter un événement</a>
+                        <a href="{{ route('structure') }}" class="w-80 ml-5 mb-3 text-xl text-center text-white rounded-lg p-5 bg-fuchsia-900">Consulter les structure</a>
+                    </div>
                 </div>
             </div>
         </div>
