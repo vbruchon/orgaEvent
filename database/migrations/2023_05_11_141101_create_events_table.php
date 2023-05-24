9294d64f06417c241c97204110a2f134d927adf4
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-05-24 10:28:07 +0200

    update event migration

--- database/migrations/2023_05_11_141101_create_events_table.php
+++ database/migrations/2023_05_11_141101_create_events_table.php
@@ -16,16 +16,16 @@ return new class extends Migration
             $table->string('name', 150);
             $table->string('partners', 400)->nullable();
             $table->text('description', 150);
-            $table->string('number_of_participants', 100);
             $table->string('location', 200)->nullable();
 
             $table->date('date_start');
             $table->date('date_end')->nullable();
-            $table->time('hours')->nullable();
+            $table->string('hours')->nullable();
             $table->text('organizer_needs')->nullable();
 
             $table->foreignId('structure_id');
             $table->foreignId('status_id');
+            $table->foreignId('number_of_participants_id', 100);
             $table->foreignId('user_id');
         });
     }
